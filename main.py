a124fa010435266ed6af868c99c393509f147cbc
Pavithra A <akkarajupavithra@gmail.com>
2024-11-12 21:22:29 +0530

    Update 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parsing import * 
 from docUpdation import *
+from keywords import *
 import asyncio
 import pymongo
 
@@ -12,13 +13,13 @@ json_list = [os.path.join(path, file) for file in os.listdir(path) if file.endsw
 for json_file in json_list:
     process_json(json_file)
 
+# Getting the pdf documents
+file_paths = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.pdf')] 
 
+
+# Inserting metadata into MongoDB Collection
 async def main():
     client = MongoClient("mongodb://localhost:27017/")  # Replace with your connection string
-     # Replace with your database and collection names
-
-    # List of PDF files to process
-    file_paths = [os.path.join(path, file) for file in os.listdir(path) if file.endswith('.pdf')]  # Replace with your file paths
 
     tasks = []
     for file_path in file_paths:
@@ -29,6 +30,22 @@ async def main():
 
     client.close()
 
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
+    
+# Getting the keywords and Updating it into the MongoDB collection
+async def main():
+
+    tasks = []
+    for file_path in file_paths:
+        task = asyncio.create_task(process_pdf(file_path, client, database_name, collection_name))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    client.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
